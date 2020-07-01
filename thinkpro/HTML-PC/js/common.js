056575b174cc31f39e2a749d3f925f8d08b793e7
hung <hung91bn@gmail.com>
2020-07-01 15:54:38 +0700

    fix
    fix

--- thinkpro/HTML-PC/js/common.js
+++ thinkpro/HTML-PC/js/common.js
@@ -212,12 +212,6 @@
 			navigation: {
 				nextEl: '.swiper-button-next',
 				prevEl: '.swiper-button-prev',
-			},
-			breakpoints: {
-				480: {
-					loop: true,
-					slidesPerView: 1,
-				}
 			}
 		});
 
